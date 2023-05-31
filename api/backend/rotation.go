87aaec1600ce91c0a335ee18119997d30b910c48
KiceAmber <kiceamber@gmail.com>
2023-05-31 16:42:23 +0800

    feat(rotation): 实现轮播图软删除功能

--- api/backend/rotation.go
+++ api/backend/rotation.go
@@ -3,7 +3,7 @@ package backend
 import "github.com/gogf/gf/v2/frame/g"
 
 type RotationReq struct {
-	g.Meta `path:"/rotation/add" tags:"Rotation" method:"post" summary:"Your first rotation api"`
+	g.Meta `path:"/backend/rotation/add" tags:"Rotation" method:"post" summary:"Your first rotation api"`
 	PicUrl string `json:"pic_url" v:"required#图片链接不能为空" dc:"图片链接"`
 	Link   string `json:"link"    v:"required#跳转链接不能为空" dc:"跳转链接"`
 	Sort   int    `json:"sort"    dc:"排序"`
@@ -11,4 +11,12 @@ type RotationReq struct {
 
 type RotationRes struct {
 	RotationId int `json:"rotation_id"`
+}
+
+type RotationDeleteReq struct {
+	g.Meta `path:"/backend/rotation/delete" method:"delete" tags:"Rotation" summary:"删除轮播图接口"`
+	Id     uint `v:"min:1#请选择需要删除的轮播图" dc:"轮播图id"`
+}
+
+type RotationDeleteRes struct {
 }
--- internal/logic/rotation/rotation.go
+++ internal/logic/rotation/rotation.go
@@ -2,7 +2,9 @@ package rotation
 
 import (
 	"context"
+	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/encoding/ghtml"
+	"github.com/gogf/gf/v2/frame/g"
 	"rime-shop-gf/internal/dao"
 	"rime-shop-gf/internal/model"
 	"rime-shop-gf/internal/service"
@@ -29,4 +31,14 @@ func (s *sRotation) Create(ctx context.Context, in model.RotationCreateInput) (o
 		return out, err
 	}
 	return model.RotationCreateOutput{RotationId: int(lastInsertID)}, err
+}
+
+func (s *sRotation) Delete(ctx context.Context, id uint) error {
+	return dao.RotationInfo.Transaction(ctx, func(ctx context.Context, tx gdb.TX) error { // TODO:
+		// 删除内容
+		_, err := dao.RotationInfo.Ctx(ctx).Where(g.Map{
+			dao.RotationInfo.Columns().Id: id,
+		}).Delete() // 使用 Unscoped() 就直接删除数据记录, 不用就是软删除数据
+		return err
+	})
 }
