c8c7b080a1ba83958abd73aaf527a845cac9247b
KiceAmber <kiceamber@gmail.com>
2023-06-19 21:50:38 +0800

    feat(role): 实现用户角色的增删改查

--- internal/logic/role/role.go
+++ internal/logic/role/role.go
@@ -2,6 +2,7 @@ package role
 
 import (
 	"context"
+	"github.com/gogf/gf/v2/frame/g"
 	"rime-shop-gf/internal/dao"
 	"rime-shop-gf/internal/model"
 	"rime-shop-gf/internal/service"
@@ -23,4 +24,44 @@ func (s *sRole) Create(ctx context.Context, in model.RoleCreateInput) (out model
 		return out, err
 	}
 	return model.RoleCreateOutput{RoleId: int(lastInsertID)}, err
+}
+
+func (s *sRole) Delete(ctx context.Context, id uint) error {
+	_, err := dao.RoleInfo.Ctx(ctx).Where(g.Map{
+		dao.RoleInfo.Columns().Id: id,
+	}).Delete()
+	return err
+}
+
+func (s *sRole) Update(ctx context.Context, in model.RoleUpdateInput) error {
+	_, err := dao.RoleInfo.
+		Ctx(ctx).Data(in).
+		FieldsEx(dao.RoleInfo.Columns().Id).
+		Where(dao.RoleInfo.Columns().Id, in.Id).
+		Update()
+	return err
+}
+
+func (s *sRole) GetList(ctx context.Context, in model.RoleGetListInput) (out *model.RoleGetListOutput, err error) {
+	//1.获得*gdb.Model对象，方便后续调用
+	m := dao.RoleInfo.Ctx(ctx)
+	//2. 实例化响应结构体
+	out = &model.RoleGetListOutput{
+		Page: in.Page,
+		Size: in.Size,
+	}
+	//3. 分页查询
+	listModel := m.Page(in.Page, in.Size)
+	//4. 再查询count，判断有无数据
+	out.Total, err = m.Count()
+	if err != nil || out.Total == 0 {
+		return out, err
+	}
+	//5. 延迟初始化list切片 确定有数据，再按期望大小初始化切片容量
+	out.List = make([]model.RoleGetListOutputItem, 0, in.Size)
+	//6. 把查询到的结果赋值到响应结构体中
+	if err := listModel.Scan(&out.List); err != nil {
+		return out, err
+	}
+	return
 }
